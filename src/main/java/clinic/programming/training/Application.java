bccacfeeb7286bc1b62209e3f0d9c9a506433ad5
tommyhlu <tommy.hlu@gmail.com>
2021-05-09 22:18:59 +0700

    some more mod

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,36 @@
 package clinic.programming.training;
 
+import java.util.List;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+
+    public int countWords(String words) {
+	String[] separatedWords = StringUtils.split(words, ' ');
+	return (separatedWords == null) ? 0 : separatedWords.length;
+    }
+
+    public void greet() {
+	List<String> greetings = new ArrayList<>();
+	greetings. add("Hello");
+
+	for (String greeting : greetings) {
+	    System.out.println("Greeting: " + greeting);
+	}
+    }
     
     public Application() {
         System.out.println ("Inside Application");
+	
     }
     
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
         System.out.println ("Starting Application");
         Application app = new Application();
+	app.greet();
+	int count = app.countWords("I have four words");
+	System.out.println("Word Count: " + count);
     }
 }
